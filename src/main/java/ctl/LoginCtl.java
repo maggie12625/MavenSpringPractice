623d4e1a3d446a43acb0a8b6174f7a2c84045ac9
吳軒穎 <a19091908@gmail.com>
2017-08-17 18:07:15 +0800

    to spring mvc 0817

---
+++ src/main/java/ctl/LoginCtl.java
@@ -0,0 +1,79 @@
+package ctl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Employee;
+import service.EmployeeService;
+
+@Controller
+public class LoginCtl {
+	
+	private EmployeeService employeeService = new EmployeeService();
+	private static final String SUCCUSS_PAGE = "main";
+	private static final String ERROR_PAGE = "login";
+	
+	@RequestMapping("/simple")
+	public   String simple(@RequestParam(required = false, value = "id") String id,
+			@RequestParam(required = false, value = "password") String password ,Model model,HttpSession session) {
+		List<String> errorMsgs = new ArrayList<String>();
+		Employee employee = new Employee();
+		employee.setEmpno(id);
+		employee.setPassword(password);
+		String path = ERROR_PAGE;
+		System.out.println("id: '" + employee.getEmpno() + "' ,pw: '" + employee.getPassword() + "'");
+		if (employee.getEmpno().equals("")) {
+			errorMsgs.add("請輸入帳號!");
+		}
+		if (employee.getPassword().equals("")) {
+			errorMsgs.add("請輸入密碼!");
+		}
+		if (!errorMsgs.isEmpty()) {
+			model.addAttribute("errorMsgs", errorMsgs);
+			return ERROR_PAGE;
+		}
+		if (employeeService.hasEmp(employee.getEmpno())) {
+			if (employeeService.checkAccount(employee)) {
+				Map<String, String> login=employeeService.getLoginInfo(employee.getEmpno());
+				model.addAttribute("login",login);
+				 
+				session.setAttribute("login",login);
+				if(login.get("end")==null){
+					path = SUCCUSS_PAGE;
+				}else{
+					errorMsgs.add("你已離職，無法登入。");
+					model.addAttribute("errorMsgs", errorMsgs);
+					model.addAttribute("employee", employee);
+				}
+					
+			} else {
+				if(employeeService.isStartedEmp(employee.getEmpno())){
+					errorMsgs.add("帳號或密碼錯誤");
+					
+				}else{
+					errorMsgs.add("帳號尚未啟動");
+				}
+				model.addAttribute("errorMsgs", errorMsgs);
+				model.addAttribute("employee", employee);
+			}
+
+		} else {
+			errorMsgs.add("查無此帳號喔!!");
+			model.addAttribute("errorMsgs", errorMsgs);
+			model.addAttribute("employee", employee);
+		}
+		 
+		return path;
+	}
+
+}
