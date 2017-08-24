72450a88091ad564532538a33cb872bd88d224a9
吳軒穎 <a19091908@gmail.com>
2017-08-24 13:40:51 +0800

    Add testing:ForgetPwd,Login

--- src/test/java/ctrl/ForgetPwdCtlTest.java
+++ src/test/java/ctrl/ForgetPwdCtlTest.java
@@ -1,28 +1,99 @@
 package ctrl;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+import org.hamcrest.core.IsNot;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import model.Employee;
+import service.EmailService;
+import service.EmployeeService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ForgetPwdCtlTest {
+	
+	MockMvc mvcMock;
+	@Mock
+	private EmployeeService employeeService;
+	@Mock
+	private EmailService service;
+	@InjectMocks
+	private ForgetPwdCtl forgetPwdCtl;
 
+    @Before
+    public void setup() {
+        InternalResourceViewResolver viewResolver = 
+        		new InternalResourceViewResolver();
+        viewResolver.setPrefix("");
+        viewResolver.setSuffix(".jsp");
+ 
+        mvcMock = standaloneSetup(forgetPwdCtl)
+                                 .setViewResolvers(viewResolver)
+                                 .build();
+    }
+    /**
+     *忘記密碼成功 
+     * */
 	@Test
 	public final void testSimple() throws Exception {
 		// TODO
-		MockMvc mvcMock = standaloneSetup(new ForgetPwdCtl()).build();
+
 		RequestBuilder requestBuilder=post("/ForgetPwd.do")
 				.param("empno", "000120")
-				.param("id", "a");
-	
+				.param("id", "A123456789");
+		
+		Employee employee=new Employee();
+		employee.setId("A123456789");
+		
+	    when(employeeService.hasEmp("000120")).thenReturn(true);
+	    when(employeeService.isStartedEmp("000120")).thenReturn(true);
+	    when(employeeService.getPersonalInfo("000120")).thenReturn(employee);
+	    
 		
 		mvcMock.perform(requestBuilder)
 		        .andExpect(status().isOk())
-		        .andExpect(view().name("forgetPwd"));
+		        .andExpect(model().attribute("result", true))
+		        .andExpect(view().name("forgetPwd"))
+		        .andExpect(forwardedUrl("forgetPwd.jsp"));
+	}
+	
+    /**
+     *忘記密碼失敗 
+     * */
+	@Test
+	public final void testSimple1() throws Exception {
+		// TODO
+
+		RequestBuilder requestBuilder=post("/ForgetPwd.do")
+				.param("empno", "000120")
+				.param("id", "A12345678");
+		
+		Employee employee=new Employee();
+		employee.setId("A123456789");
+		
+	    when(employeeService.hasEmp("000120")).thenReturn(true);
+	    when(employeeService.isStartedEmp("000120")).thenReturn(true);
+	    when(employeeService.getPersonalInfo("000120")).thenReturn(employee);
+	    
+		
+		mvcMock.perform(requestBuilder)
+		        .andExpect(status().isOk())
+		        .andExpect(model().attribute("result",false))
+		        .andExpect(view().name("forgetPwd"))
+		        .andExpect(forwardedUrl("forgetPwd.jsp"));
 	}
 
 }
--- src/test/java/ctrl/LoginCtlTest.java
+++ src/test/java/ctrl/LoginCtlTest.java
@@ -2,95 +2,79 @@ package ctrl;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
-import java.sql.SQLException;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
-
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import dao.ConnectionHelper;
-import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+import service.EmployeeService;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class LoginCtlTest {
-	@Before
-	public void contextSetup() throws Exception {		
-//	    try {
-//
-//	        // Create initial context
-//	        System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
-//	            "org.apache.naming.java.javaURLContextFactory");
-//	        System.setProperty(Context.URL_PKG_PREFIXES, 
-//	            "org.apache.naming");            
-//	        InitialContext ic = new InitialContext();
-//
-//	        ic.createSubcontext("java:");
-//	        ic.createSubcontext("java:/comp");
-//	        ic.createSubcontext("java:/comp/env");
-//	        ic.createSubcontext("java:/comp/env/jdbc");
-//
-//	        // Construct DataSource
-//	        OracleConnectionPoolDataSource ds = new OracleConnectionPoolDataSource();
-//	        ds.setURL("jdbc:oracle:thin:@//200.200.200.200:49161/xe"); // <--insert url to database here
-//	        ds.setUser("system"); //<-- self explanatory
-//	        ds.setPassword("oracle"); //<-- self explanatory
-//	        
-//            ic.bind("java:comp/env/jdbc/fjut5_3_wt_ds", ds); //<--insert name of binding here
-//            
-//	    } catch (NamingException ex) {
-//	        ex.printStackTrace();
-//	    } catch (SQLException ex){
-//	        ex.printStackTrace();
-//	    }
-		
-//		Context initContext = new InitialContext();
-//		Context envContext = (Context) initContext.lookup("java:/comp/env");
-//		DataSource dataSource = (DataSource) envContext.lookup("jdbc/fjut5_3_wt_ds");
-//		SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
-//		builder.bind("java:comp/env/jdbc/fjut5_3_wt_ds", dataSource);
-//		builder.activate();
-	}
-	//登入成功
+	
+	MockMvc mvcMock;
+	@Mock
+	private EmployeeService employeeService;
+	@InjectMocks
+	private LoginCtl loginCtl;
+
+	
+    @Before
+    public void setup() {
+        InternalResourceViewResolver viewResolver = 
+        		new InternalResourceViewResolver();
+        viewResolver.setPrefix("");
+        viewResolver.setSuffix(".jsp");
+ 
+        mvcMock = standaloneSetup(loginCtl)
+                                 .setViewResolvers(viewResolver)
+                                 .build();
+    }
+	/**
+	 * 登入成功
+	 * */
 	@Test
 	public void testSimple() throws Exception {
-		
-		MockMvc mvcMock = standaloneSetup(new LoginCtl()).build();
+
 		RequestBuilder requestBuilder=post("/Login.do")
 				.param("id", "000000")
 				.param("password", "a");
 		
-		mvcMock.perform(requestBuilder)
-		        .andExpect(status().isOk())
-		        .andExpect(view().name("main"));
+		when(employeeService.hasEmp(anyString())).thenReturn(true);
+		when(employeeService.checkAccount(any())).thenReturn(true);
+		when(employeeService.getLoginInfo(anyString())).thenReturn(anyMap());
+
+        mvcMock.perform(requestBuilder)
+		       .andExpect(status().isOk())
+		       .andExpect(view().name("main"))
+		       .andExpect(forwardedUrl("main.jsp"));
 	}
-	//登入錯誤
+	/**
+	 * 登入錯誤
+	 * */
 	@Test
 	public void testSimple1() throws Exception {
 		
-		MockMvc mvcMock = standaloneSetup(new LoginCtl()).build();
 		RequestBuilder requestBuilder=post("/Login.do")
-				.param("id", "000000")
+				.param("id", "abcde")
 				.param("password", "a");
 		
-		String text=mvcMock.perform(requestBuilder)
+		when(employeeService.hasEmp(anyString())).thenReturn(false);
+		
+        mvcMock.perform(requestBuilder)
 		        .andExpect(status().isOk())
 		        .andExpect(view().name("login"))
-		        .andReturn().getResponse().getContentAsString();
+		        .andExpect(forwardedUrl("login.jsp"));
 	
-		System.out.println("text:"+text);
 	}
 
 }
